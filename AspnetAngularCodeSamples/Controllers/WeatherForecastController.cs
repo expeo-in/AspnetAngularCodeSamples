6f335a6c7e880c116b0b2c01b2b2e12ef34982cd
vijay <vijay@DESKTOP-04NMTUO>
2022-01-16 15:15:42 +0530

    Add project files.

---
+++ AspnetAngularCodeSamples/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetAngularCodeSamples.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWebHostEnvironment env;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            this.env = env;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Download")]
+        public IActionResult Download()
+        {
+            string csvFilePath = Path.Combine(env.WebRootPath, "Weather.csv");
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var format = new ExcelTextFormat();
+            format.Delimiter = ',';
+            //format.EOL = "\n";              // DEFAULT IS "\r\n";
+            format.TextQualifier = '"';
+
+            using var stream = new MemoryStream();
+            using (var package = new ExcelPackage(stream))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Data");
+                worksheet.Cells["A1"].LoadFromText(new FileInfo(csvFilePath), format, OfficeOpenXml.Table.TableStyles.Medium27, FirstRowIsHeader: true);
+                package.Save();
+            }
+            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+    }
+}
